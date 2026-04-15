98968644e26d54d4efb48700af96d3354b8c7a81
Alberto <1123alberto@users.noreply.github.com>
2026-04-15 23:54:45 +0300

    feat: implement PWA with manifest, service worker and icons

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'moshopoulos-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './manifest.json',
+  './icons/dental-implant-circle-256-torqued.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
